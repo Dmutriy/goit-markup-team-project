7ff7df5a13a20e1778894b45786e8a1077ffdfe1
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2022-07-19 06:50:37 +0000

    Deploying to gh-pages from @ Dmutriy/goit-markup-team-project@f8745d8d89468961f98d084a9fccc85a5f5167f5 🚀

---
+++ index.0c26ffd4.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector(".menu-open-btn"),closeMenuBtn:document.querySelector(".menu-close-btn"),menu:document.querySelector(".mob-menu-backdrop")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.0c26ffd4.js.map
