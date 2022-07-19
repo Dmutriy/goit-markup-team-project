f8745d8d89468961f98d084a9fccc85a5f5167f5
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2022-07-19 09:49:54 +0300

    hero header 9:49

---
+++ src/menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.menu-open-btn'),
+    closeMenuBtn: document.querySelector('.menu-close-btn'),
+    menu: document.querySelector('.mob-menu-backdrop'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
